7dddc3c096d048e6b7b15f7471ffc7975aaa567f
lym0909 <pgwy9990@gmail.com>
2015-11-09 20:29:36 +0900

    이미지 업로드
    werkzeug를 이용한 서버에 이미지 업로드 완성

--- flask_test.py
+++ flask_test.py
@@ -3,15 +3,20 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+import os
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import session
 from flask import redirect
 from flask import url_for
+from werkzeug import secure_filename
 from translate import translate_ms
 
+UPLOAD_FOLDER = './image'
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/translate/<lang_from>/<lang_to>/<sentence>')
 def returnTrans(lang_from, lang_to, sentence):
@@ -23,6 +28,10 @@ def returnTrans():
         from_lang = request.form['from_lang']
         to_lang = request.form['to_lang']
         string = request.form['string']
+        
+        img = request.files['img']
+        imgName = secure_filename(img.filename)
+        img.save(os.path.join(app.config['UPLOAD_FOLDER'], imgName))
 
     return translate_ms(string, from_lang, to_lang)
 
